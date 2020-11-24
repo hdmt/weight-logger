23c9fb5ac884d8911b7cba744305139f705abf0b
hdmt <tanaka@trsp.biz>
2020-11-24 22:15:26 +0900

    init

---
+++ src/components/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    render(){
+      return(
+        <div>
+          <h1>this is Dashboard</h1>
+          
+        </div>
+      )
+    }
+  }
+  
+  
+  export default Dashboard;
